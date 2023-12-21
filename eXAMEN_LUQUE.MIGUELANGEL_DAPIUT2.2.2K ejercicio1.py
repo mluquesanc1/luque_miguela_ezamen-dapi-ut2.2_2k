ba079b69ea0126756dec1d56369aa228b5cc3129
mluquesanc1 <mluquesanc1@educacion.navarra.es>
2023-12-21 16:44:27 +0100

    exMEN

---
+++ eXAMEN_LUQUE.MIGUELANGEL_DAPIUT2.2.2K ejercicio1.py
@@ -0,0 +1,8 @@
+
+from io import (lista)"""iniciop de lo que quiera que estoy haciendo"""
+""" creamos  el arjchivo txt de nose que"""
+compañeros=("pagados.txt", "w")
+compañeros.write("manolo,pepe,maria.jum") 
+"""cerramos archivos el archivo"""
+
+compañeros.closset
---
+++ eXAMEN_LUQUE.MIGUELANGEL_DAPIUT2.2.2K ejercicio2.py
@@ -0,0 +1,6 @@
+milista = ("[km0, 10:00], [km8,10:15], [km16,10.30], [km25, 11:00]:")
+milista = corredor
+milista=("corredor", [milista])
+"""esto muesta la fulmision de la cosasa extraña"""
+print = corredor[:]
+  
